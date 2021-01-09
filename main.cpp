0f8d373f192547b8f38f89319682d06d18f48bcf
burikunayoni <dabrixpert@gmail.com>
2021-01-09 22:52:38 +0800

    INDEV_PROGRESS_21/01/09_A

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ int main()
     srand(time(NULL));
     while(EOF)
     {
+        system("cls");
         title();
         stop = getch();
         if(stop == 32)
@@ -19,16 +20,60 @@ int main()
     system("pause");
     return 0;
 }
-void game()
+void game()//game
 {
-    int x, y;
-    cin>>x>>y;
+    system("cls");
+    int x, y, mines;
+    int m_coordinate_x, m_coordinate_y;
+    /*
+    -------> X AXIS
+    |
+    |
+    | Y AXIS
+    |
+    V
+    */
+    cin>>x>>y>>mines;
     int map[x][y];
-
+    //initialize(id = 0)
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            map[i][j] = 0;
+        }
+    }
+    //set mines(id = 9)
+    for (int i = 1; i <= mines; i++)
+    {
+        m_coordinate_x = rand() % 9;
+        m_coordinate_y = rand() % 9;
+        map[m_coordinate_x][m_coordinate_y] = 9;
+        //set number around mines.
+        map[m_coordinate_x-1][m_coordinate_y-1]++;//up-left
+        map[m_coordinate_x][m_coordinate_y-1]++;//up-mid
+        map[m_coordinate_x+1][m_coordinate_y-1]++;//up-right
+        map[m_coordinate_x-1][m_coordinate_y]++;//mid-left
+        map[m_coordinate_x][m_coordinate_y]++;//mid-mid
+        map[m_coordinate_x+1][m_coordinate_y]++;//mid-right
+        map[m_coordinate_x-1][m_coordinate_y+1]++;//down-left
+        map[m_coordinate_x][m_coordinate_y+1]++;//down-mid
+        map[m_coordinate_x+1][m_coordinate_y+1]++;//down-right
+        //conflict between mines still unfix. ---LOG_210109.
+    }
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            cout<<map[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    system("pause");
 }
 void title()
 {
-    printf("VER: INDEV_PROG_210107A\n\n");
+    printf("VER: INDEV_PROG_210109A\n\n");
     printf("  _                   _  _\n");
     printf(" | |__   _   _  _ __ (_)| | __ _   _  ___ __      __ ___   ___  _ __    ___  _ __\n");
     printf(" | '_ \\ | | | || '__|| || |/ /| | | |/ __|\\ \\ /\\ / // _ \\ / _ \\| '_ \\  / _ \\| '__|\n");
